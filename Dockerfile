894968a765084da0309c5e5ee0b3df571830cc54
Krzysztof Sikora <226458@student.uek.krakow.pl>
2024-11-04 14:35:29 +0100

    Uzupelnienie zajec

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,5 @@ RUN pip install -r requirements.txt
 ENTRYPOINT [ "python", "./manage.py", "runserver", "0.0.0.0:8000" ]
 
 EXPOSE 8000
-COPY src .
+COPY src . 
 
--- src/kernel/settings.py
+++ src/kernel/settings.py
@@ -20,15 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fzd%b=1x)#8@ve31li$v%vna-n61jlqf+*uvy+*q%kgitto^z3'
+SECRET_KEY = 'django-insecure-7=0f-apiw6kaje509mx(t_$ye&u@%lfow0@p(imbn%i(bub-9g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-print("hello world")
-
 
 # Application definition
 
